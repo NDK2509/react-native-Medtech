8ae409d87d315948ad476de1cf08a5155d126861
NDK2509 <Dangkyqt2509@gmail.com>
2022-11-14 07:06:30 +0700

    [Update] deal of the day component

--- src/components/home/DealOfTheDay.tsx
+++ src/components/home/DealOfTheDay.tsx
@@ -1,19 +1,27 @@
 import { FlatList, View, Text, TouchableOpacity } from "react-native"
+import { ProductType } from "../../api/ProductsAPI"
 import colors from "../../theme/colors"
-import Product, { ProductProps } from "../base/Product"
+import NoDataText from "../base/NoDataText"
+import Product from "../base/Product"
 import SmallTitle from "../base/SmallTitle"
 
-export default (props: { data: Array<ProductProps> }) => {
-  const _renderItem = ({ item } : {item: ProductProps}) => <Product {...item} />
+export default (props: { data: Array<ProductType> }) => {
+  const _renderItem = ({ item } : {item: ProductType}) => <Product {...item} />
   return (
-    <View style={{ marginVertical: 20 }}>
-      <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
-        <SmallTitle title="Deal of the day" />
-        <TouchableOpacity>
-          <Text style={{ color: colors.primaryBlue, marginEnd: 20 }}>See more</Text>
-        </TouchableOpacity>
+    props.data.length ? (
+      <View style={{ marginVertical: 20 }}>
+        <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+          <SmallTitle title="Deal of the day" />
+          <TouchableOpacity>
+            <Text style={{ color: colors.primaryBlue, marginEnd: 20 }}>See more</Text>
+          </TouchableOpacity>
+        </View>
+        <FlatList data={props.data} renderItem={_renderItem} horizontal={true} showsHorizontalScrollIndicator={false} keyExtractor={ (item) => item.id } />
       </View>
-      <FlatList data={props.data} renderItem={_renderItem} horizontal={true} showsHorizontalScrollIndicator={false} />
-    </View>
+    )
+    : (
+        <NoDataText title="There is no data of product!"/>
+    )
+    
   )
 }
